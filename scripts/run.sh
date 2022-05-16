d5cce72d11e82250d8c978bc5b754a074f676bd8
Gavin Roberts <gavinsroberts13@gmail.com>
2022-05-15 17:20:33 -0700

    Switched to MAVProxy implimentation

---
+++ scripts/run.sh
@@ -0,0 +1,2 @@
+source ./venv/bin/activate
+mavproxy.py --master=/dev/tty.usbmodem01 --baudrate=57600
---
+++ scripts/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+echo "Creating virtual environment..."
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    virtualenv --python=/Library/Frameworks/Python.framework/Versions/3.9/bin/python3 ./venv
+    source ./venv/bin/activate
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    virtualenv --python=/Library/Frameworks/Python.framework/Versions/3.9/bin/python3 ./venv
+    source ./venv/bin/activate
+else
+    echo "ERROR: $OSTYPE is not currently supported."
+    exit 1
+fi
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Installation completed successfully"
+run.sh
