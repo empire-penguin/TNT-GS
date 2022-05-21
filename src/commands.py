923fefa45ad25d572f541f788ff1883a02dfda75
Gavin Roberts <gavinsroberts13@gmail.com>
2022-05-21 15:32:19 -0700

    refactored commands

--- src/commands.py
+++ src/commands.py
@@ -17,3 +17,12 @@ def disarm_drone(connection):
         mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 
         0, 0, 0, 0, 0, 0, 0, 0
     )
+
+def take_off(connection):
+    print("TAKING OFF") 
+    connection.mav.command_long_send(
+        connection.target_system, 
+        connection.target_component, 
+        mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 
+        0, 1, 0, 0, 0, 0, 0, 5
+    )
