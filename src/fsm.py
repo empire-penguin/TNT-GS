604babee1bb4a9e885116ba5fc66c81148074ef2
NihalBNazeem <69162156+NihalBNazeem@users.noreply.github.com>
2022-05-27 19:01:59 -0700

    Update fsm.py
    General Layout, need to add keyboard interrupt and mavlink functions

--- src/fsm.py
+++ src/fsm.py
@@ -1,1 +1,31 @@
 #Implementing FSM
+
+BUFFER = 0
+LEFT = 1
+RIGHT = 2
+UP = 3
+DOWN = 4
+INTERRUPT = #SPACE KEYINPUT TODO
+
+while(! INTERRUPT):
+  def buffer():
+    return #TODO
+  def left():
+    return #TODO
+  def right():
+    return #TODO
+  def up():
+    return #TODO
+  def down():
+    return #TODO
+
+switcher = {
+    BUFFER: buffer,
+    LEFT: left,
+    RIGHT: right,
+    UP: up,
+    DOWN: down,
+}
+
+def switch(input_num):
+    return switcher.get(input_num, default)()
