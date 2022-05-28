e2c13e9366ec538b6362302d743117e0161b9e69
rizwan3711 <84203059+rizwan3711@users.noreply.github.com>
2022-05-27 22:59:07 -0700

    resolved merge

--- src/fsm.py
+++ src/fsm.py
@@ -26,7 +26,6 @@ UP = 3
 DOWN = 4
 INTERRUPT = #SPACE KEYINPUT TODO
 
-<<<<<<< HEAD
 async def run():
 
     origin = [0,0,0] #for land command
@@ -73,7 +72,6 @@ async def run():
         print("-- Disarming")
         await drone.action.disarm()
         return
-=======
 #TODO, change Rotation
 
 #North, East, Down, Degrees toward East
@@ -81,7 +79,6 @@ CurrLoc = (0,0,0,0)
 XOffset = 5;
 YOffset = 5;
 ZOffset = 5;
->>>>>>> 52ea0aa5dda25f25da3628246375dfbef72bed7a
 
 while(! INTERRUPT):
   def buffer():
