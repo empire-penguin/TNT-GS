a9f0a5be532939439b51ff06027d26be9d12656c
Mimicradel <cassia.rizq@gmail.com>
2022-05-28 16:37:23 -0700

    added keyboard input functionality

--- src/connection_test.py
+++ src/connection_test.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     parent_conn,child_conn = Pipe()
     p = Process(target=f, args=(child_conn,))
     p.start()
-    print(parent_conn.recv())   # prints input
+    print("Output: ",parent_conn.recv())   # prints input
--- src/user_input.py
+++ src/user_input.py
@@ -1,24 +1,20 @@
-from multiprocessing import Process,Pipe
+from multiprocessing import Process, Pipe
 
 def user_input(input):
     try:
         # Convert it into integer
         val = int(input)
         print("Input is an integer number. Number = ", val)
-        #Add Dictionary Mappinng for num to gesture in user output?
+        #Add Dictionary Mapping for num to gesture in user output?
     except ValueError:
             print("Not an integer")
     else:
-        if input in range(0,6):
+        if val in range(0,6):
           pass
         else:
           print("Please type an integer in [0,5]")
 
-
-val = input("Enter an Integer")
-user_input(input1)
-
 def f(child_conn):
-    msg = val
+    msg = user_input(input("Enter an Integer: "))
     child_conn.send(msg)
     child_conn.close()
